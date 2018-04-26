43e892e9344fa57beef75f5a1bd36ef6bab2ea40
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-04-26 19:38:16 -0400

    random forest implementation for all targets

--- sayan_random_forest.R
+++ sayan_random_forest.R
@@ -11,7 +11,7 @@
 rm(list=ls())
 #################################################
 
-# Convert to factors for Random Tree
+# Convert to factors for Random Forest
 
 library(dplyr)
 storm_data_relevant=storm_data_relevant %>% mutate_if(is.character, as.factor)
@@ -26,12 +26,50 @@ index <- sort(sample(nrow(storm_data_relevant), round(.30*nrow(storm_data_releva
 training <- storm_data_relevant[-index,]
 test <- storm_data_relevant[index,]
 
-rForest <- randomForest(INJURIES~., data=training, importance=TRUE, ntree=1000)
-varImpPlot(rForest)
+# RF for Injuries
+rForest_Injuries <- randomForest(INJURIES~., data=training, importance=TRUE, ntree=1000)
 
-prediction <- predict(rForest, test)
-wrong <- (test[,"INJURIES"] != prediction)
+prediction_injuries <- predict(rForest_Injuries, test)
+table(Prediction = prediction_injuries, Actual = test$INJURIES)
+
+wrong <- (test[,"INJURIES"] != prediction_injuries)
 error_rate <- sum(wrong)/length(wrong)
+error_rate * 100
 
-# table(Prediction = prediction, Actual = test$INJURIES)
-# importance(rForest)
+varImpPlot(rForest_Injuries)
+
+# RF for Deaths
+rForest_Deaths <- randomForest(DEATHS~., data=training, importance=TRUE, ntree=1000)
+
+prediction_deaths <- predict(rForest_Deaths, test)
+table(Prediction = prediction_deaths, Actual = test$DEATHS)
+
+wrong <- (test[,"DEATHS"] != prediction_deaths)
+error_rate <- sum(wrong)/length(wrong)
+error_rate * 100
+
+varImpPlot(rForest_Deaths)
+
+# RF for Damage Property
+rForest_Property <- randomForest(DAMAGE_PROPERTY~., data=training, importance=TRUE, ntree=1000)
+
+prediction_property <- predict(rForest_Property, test)
+table(Prediction = prediction_property, Actual = test$DAMAGE_PROPERTY)
+
+wrong <- (test[,"DAMAGE_PROPERTY"] != prediction_property)
+error_rate <- sum(wrong)/length(wrong)
+error_rate * 100
+
+varImpPlot(rForest_Property)
+
+# RF for Damage Crops
+rForest_Crops <- randomForest(DAMAGE_CROPS~., data=training, importance=TRUE, ntree=1000)
+
+prediction_crops <- predict(rForest_Crops, test)
+table(Prediction = prediction_crops, Actual = test$DAMAGE_CROPS)
+
+wrong <- (test[,"DAMAGE_CROPS"] != prediction_crops)
+error_rate <- sum(wrong)/length(wrong)
+error_rate * 100
+
+varImpPlot(rForest_Crops)
